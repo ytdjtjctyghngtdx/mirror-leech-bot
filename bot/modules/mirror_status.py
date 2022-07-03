c4f9b97bb426c986a3318db0a5f33d34ab1b48d3
woodcraft5 <sekhfaruk5@gmail.com>
2022-07-03 22:23:51 +0530

    mirror_status.py

--- bot/modules/mirror_status.py
+++ bot/modules/mirror_status.py
@@ -14,7 +14,7 @@ def mirror_status(update, context):
         if len(download_dict) == 0:
             currentTime = get_readable_time(time() - botStartTime)
             free = get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)
-            message = '🌼 Nothing To Do ! 🌼\n_______________'
+            message = '🌼 Nothing To Do ! 🌼\n✧✧✧✧𝐖𝐎𝐎𝐃𝐜𝐫𝐚𝐟𝐭✧✧✧✧'
             message += f"\n\n<b>➥ CPU ➠       :</b> {cpu_percent()}%\n<b>➥ SSD ➠       :</b> {get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)}" \
                        f"\n<b>➥ RAM ➠      :</b> {virtual_memory().percent}%\n<b>➥ UPTM ➠    :</b> {get_readable_time(time() - botStartTime)}"
             reply_message = sendMessage(message, context.bot, update.message)
